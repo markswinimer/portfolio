2ed46c2657c6fa767ae653972e2701a94a2c2edd
markswinimer <markswinimer@gmail.com>
2019-11-12 23:23:05 -0500

    styled media queries begin

--- src/ContentSection.js
+++ src/ContentSection.js
@@ -7,14 +7,13 @@ class ContentSection extends Component {
       <div className={`ContentSection-container ${this.props.color}`}>
         <div className="ContentSection">
           <div className="ContentSection-left">
-            <h2>Recent</h2>
-            <ul>
-              <li>React</li>
-              <li>ES6 Javascript</li>
-              <li>Ruby on Rails</li>
-              <li>WordPress</li>
-              <li>HTML & CSS</li>
-            </ul>
+          <div className="ContentSection-tech">
+              <p>React</p>
+              <p>ES6 Javascript</p>
+              <p>HTML & CSS</p>
+              <p>Ruby on Rails</p>
+              <p>WordPress</p>
+          </div>
           </div>
           <div className="ContentSection-right">
             <h1>{header}</h1>
