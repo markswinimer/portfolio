da347b8078332c51ad6b2a854a6aba8e040f7181
markswinimer <markswinimer@gmail.com>
2019-11-13 22:24:21 -0500

    fixes some mediaq

--- src/ContentSection.js
+++ src/ContentSection.js
@@ -7,13 +7,13 @@ class ContentSection extends Component {
       <div className={`ContentSection-container ${this.props.color}`}>
         <div className="ContentSection">
           <div className="ContentSection-left">
-          <div className="ContentSection-tech">
-              <p>React</p>
-              <p>ES6 Javascript</p>
-              <p>HTML & CSS</p>
-              <p>Ruby on Rails</p>
-              <p>WordPress</p>
-          </div>
+            <div className="ContentSection-tech">
+                <p>React</p>
+                <p>ES6 Javascript</p>
+                <p>HTML & CSS</p>
+                <p>Ruby on Rails</p>
+                <p>WordPress</p>
+            </div>
           </div>
           <div className="ContentSection-right">
             <h1>{header}</h1>
